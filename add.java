a9a9b54015b74edef62d1bdad513c93a012aa9c7
pavithra200595 <pavipavi491@gmail.com>
2023-03-02 10:10:33 +0530

    first commit

---
+++ add.java
@@ -0,0 +1,17 @@
+class arithmetic
+{
+    public static void main(String args[])
+     {
+        int a=20;
+        int b=10;
+        System.out.println("The addition of "+a+" and " +b+ " is: "+(a+b));
+        System.out.println("The subraction of "+a+" and " +b+ " is: "+(a-b));
+        System.out.println("The multiplication of "+a+" and " +b+ " is: "+(a*b));
+        System.out.println("The division of "+a+" and " +b+ " is: "+(a/b));
+        System.out.println("The modulous of "+a+" and " +b+ " is: "+(a%b));
+}
+}
+        
+
+
+
